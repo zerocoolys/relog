6df164c18ec5affb21f8f73e77c7f7f593a53b69
dolphineor <dolphineor@gmail.com>
2015-05-14 18:59:09 +0800

    SEM的搜索关键词解析

--- src/main/java/com/ss/main/Constants.java
+++ src/main/java/com/ss/main/Constants.java
@@ -30,7 +30,7 @@ public interface Constants {
     String UCV = "_ucv";    // 访客数(UV)区分标识符
     String RF = "rf";
     String SE = "se";
-    String KW = "kw";
+    String KW = "kw";       // 搜索词
     String RF_TYPE = "rf_type";     // 1. 直接访问, 2. 搜索引擎, 3. 外部链接
     String DOMAIN = "dm";
 
@@ -39,5 +39,22 @@ public interface Constants {
     String ET_ACTION = "action";    // 与目标的交互行为
     String ET_LABEL = "label";      // 事件的额外信息
     String ET_VALUE = "value";      // 事件的额外数值信息
+
+    String KEYWORD_INFO_REQUEST_URL = "http://182.92.227.79:9080/user/%s/keyword/%s";
+    String SEM_ACCOUNT_ID = "accountId";
+    String SEM_CAMPAIGN_ID = "campaignId";
+    String SEM_CAMPAIGN_NAME = "campaignName";
+    String SEM_ADGROUP_ID = "adgroupId";
+    String SEM_ADGROUP_NAME = "adgroupName";
+    String SEM_KEYWORD_NAME = "keywordName";
+    String SEM_KEYWORD_IDENTIFIER = "ca";
+
+    String ES_ACCOUNT_ID = "acid";
+    String ES_CAMPAIGN_ID = "cid";
+    String ES_CAMPAIGN_NAME = "cpna";
+    String ES_ADGROUP_ID = "agid";
+    String ES_ADGROUP_NAME = "agna";
+    String ES_KEYWORD_ID = "kwid";
+    String ES_KEYWORD_NAME = "kwna";
 
 }
