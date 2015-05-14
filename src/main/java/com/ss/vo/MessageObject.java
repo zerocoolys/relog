7f0d87199785d9e0dd016cfe264a31e83090041f
YouSheng <vbzerocool@gmail.com>
2015-05-14 14:48:30 +0800

    [ys_path_split] 将查询的loc去除http头部,以及url带"?"的后部分

--- src/main/java/com/ss/vo/MessageObject.java
+++ src/main/java/com/ss/vo/MessageObject.java
@@ -1,5 +1,6 @@
 package com.ss.vo;
 
+import com.ss.main.Constants;
 import io.netty.handler.codec.http.HttpRequest;
 
 import java.util.HashMap;
@@ -8,7 +9,7 @@ import java.util.Map;
 /**
  * Created by yousheng on 15/3/17.
  */
-public class MessageObject {
+public class MessageObject implements Constants {
 
     private HttpRequest httpMessage;
 
@@ -33,4 +34,26 @@ public class MessageObject {
     public Map<String, Object> getAttribute() {
         return attribute;
     }
+
+
+    public MessageObject method(String method) {
+        attribute.put(METHOD, method);
+        return this;
+    }
+
+    public MessageObject version(String version){
+        attribute.put(VERSION, version);
+        return this;
+    }
+
+    public MessageObject remote(String remote){
+        attribute.put(REMOTE,remote);
+        return this;
+    }
+
+    public MessageObject kv(String key , String value){
+        attribute.put(key,value);
+        return this;
+    }
+
 }
