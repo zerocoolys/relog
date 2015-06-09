5a0258b19250b8706630d41b3ba08f75ce742249
dolphineor <dolphineor@gmail.com>
2015-06-09 18:53:43 +0800

    update Consumer and Producer Main

--- src/main/java/com/ss/main/RelogConsumerMain.java
+++ src/main/java/com/ss/main/RelogConsumerMain.java
@@ -3,6 +3,7 @@ package com.ss.main;
 import com.ss.es.EsForward;
 import com.ss.es.EsPools;
 import com.ss.mq.consumer.HLConsumerGroup;
+import org.elasticsearch.client.transport.TransportClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +12,25 @@ import java.util.List;
  * Created by dolphineor on 2015-5-27.
  */
 public class RelogConsumerMain {
+
     public static void main(String[] args) {
         RelogConfig.setMode(args[0]);
         RelogConfig.setKafkaTopic(args[1]);
         EsPools.setBulkRequestNumber(Integer.parseInt(args[2]));
         int consumerThreadNumber = Integer.valueOf(args[3]);
 
+        List<TransportClient> esClients = new ArrayList<>();
         List<EsForward> esForwards = new ArrayList<>();
-        EsPools.getEsClient().forEach(client -> esForwards.add(new EsForward(client)));
+        EsPools.getEsClient().forEach(client -> {
+            esClients.add(client);
+            esForwards.add(new EsForward(client));
+        });
 
         new HLConsumerGroup(RelogConfig.getKafkaTopic(), consumerThreadNumber, esForwards);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            esClients.forEach(TransportClient::close);
+        }));
 
         try {
             Thread.currentThread().join();
